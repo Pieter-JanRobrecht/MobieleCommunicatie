d3bd3fc9afc2f4520028eeda56de583e78ffe300
fake <fake@fake.com>
2016-10-10 09:15:34 +0000

    Aanpassing voor temp door te sturen

--- raspberry/arduino_to_pi/arduino_to_pi.ino
+++ raspberry/arduino_to_pi/arduino_to_pi.ino
@@ -1,13 +1,18 @@
 char dataString[50] = {0};
-int a =0; 
+int a =0;
+const int TEMP = 11;
+int sensorValue=0;
 
 void setup() {
-Serial.begin(9600);              //Starting serial communication
+  Serial.begin(9600);             //Starting serial communication
+  pinMode(TEMP,INPUT);
 }
   
 void loop() {
-  a++;                          // a value increase every loop
-  sprintf(dataString,"%02X",a); // convert a value to hexa 
-  Serial.println(dataString);   // send the data
-  delay(1000);                  // give the loop some break
+  sensorValue = analogRead(TEMP);
+  Serial.println(sensorValue,BIN); //Data verzonden in bit vorm laatste 4 bits zijn na de komma
+//  a++;                          // a value increase every loop
+//  sprintf(dataString,"%02X",a); // convert a value to hexa 
+//  Serial.println(dataString);   // send the data
+  delay(3000);                  // give the loop some break
 }
