eecf330d84ffa322ee1b4480768979fded25cdf6
TimVV1 <tim_van_vooren@hotmail.com>
2016-12-10 21:31:19 +0100

    Add files via upload

---
+++ raspberry/pi_to_arduino/sunrise2.py
@@ -0,0 +1,11 @@
+import serial
+import time
+
+ser = serial.Serial('COM5', 9600, timeout = 1)
+while True:
+    ser.write(22)
+    sunrise = ser.readline()
+    print "Inkomend signaal " 
+    if(sunrise != ''):
+        print "sunrise = "+ sunrise
+        break
