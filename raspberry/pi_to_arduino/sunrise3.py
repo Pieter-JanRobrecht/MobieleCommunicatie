e3d35057e0db206c9d07ee59e91fe23c5c6bc997
TimVV1 <tim_van_vooren@hotmail.com>
2016-12-10 21:51:49 +0100

    Update sunrise3.py

--- raspberry/pi_to_arduino/sunrise3.py
+++ raspberry/pi_to_arduino/sunrise3.py
@@ -3,7 +3,7 @@ import time
 
 ser = serial.Serial('COM5', 9600, timeout = 1)
 while True:
-    ser.write(33)
+    ser.write('33')
     sunrise = ser.readline()
     print "Inkomend signaal " 
     if(sunrise != ''):
