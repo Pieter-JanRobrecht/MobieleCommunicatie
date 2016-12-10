ae644b47d0ad85a3f06c17f65d26777f2ba1579f
TimVV1 <tim_van_vooren@hotmail.com>
2016-12-10 14:53:34 +0100

    Add files via upload

---
+++ raspberry/arduino_to_pi/Light_&_Temp_from_arduino_to_pi.py
@@ -0,0 +1,56 @@
+import serial
+import time
+
+ser = serial.Serial('COM3', 9600, timeout=1)
+#while True:
+#ser.write('sensors')
+
+while True:
+    ser.write('temp1')
+    temp1 = ser.readline()
+    print "Inkomend signaal " 
+    if(temp1 != ''):
+        print "temp1 = "+ temp1
+        break
+
+while True:
+    ser.write('temp2')
+    temp2 = ser.readline()
+    print "Inkomend signaal " 
+    if(temp2 != ''):
+        print "temp2 = "+ temp2
+        break
+    
+while True:
+    ser.write('temp3')
+    temp3 = ser.readline()
+    print "Inkomend signaal " 
+    if(temp3 != ''):
+        print "temp3 = "+ temp3
+        break
+
+while True:
+    ser.write('light1')
+    light1 = ser.readline()
+    print "Inkomend signaal " 
+    if(light1 != ''):
+        print "light1 = "+ light1
+        break
+
+while True:
+    ser.write('light2')
+    light2 = ser.readline()
+    print "Inkomend signaal " 
+    if(light2 != ''):
+        print "light2 = "+ light2
+        break
+
+while True:
+    ser.write('light3')
+    light3 = ser.readline()
+    print "Inkomend signaal " 
+    if(light3 != ''):
+        print "light3 = "+ light3
+        ser.close()
+        break
+ser.close()
