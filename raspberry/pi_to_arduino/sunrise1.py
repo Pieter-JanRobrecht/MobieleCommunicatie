47b7384091be96a28b6c2bbd7c2c9a1e82983b58
Pieter-Jan Robrecht <pieterjan.robrecht@student.kuleuven.be>
2016-12-12 10:50:39 +0100

    demo scripts

--- raspberry/pi_to_arduino/sunrise1.py
+++ raspberry/pi_to_arduino/sunrise1.py
@@ -2,10 +2,10 @@ import serial
 import time
 
 ser = serial.Serial('COM5', 9600, timeout = 1)
-while True:
-    ser.write('11')
-    sunrise = ser.readline()
-    print "Inkomend signaal " 
-    if(sunrise != ''):
-        print "sunrise = "+ sunrise
-        break
+#while True:
+ser.write('11')
+#    sunrise = ser.readline()
+ #   print "Inkomend signaal " 
+  #  if(sunrise != ''):
+   #     print "sunrise = "+ sunrise
+    #    break
--- raspberry/pi_to_arduino/sunrise2.py
+++ raspberry/pi_to_arduino/sunrise2.py
@@ -2,10 +2,10 @@ import serial
 import time
 
 ser = serial.Serial('COM5', 9600, timeout = 1)
-while True:
-    ser.write('22')
-    sunrise = ser.readline()
-    print "Inkomend signaal " 
-    if(sunrise != ''):
-        print "sunrise = "+ sunrise
-        break
+#while True:
+ser.write('22')
+#    sunrise = ser.readline()
+ #   print "Inkomend signaal " 
+  #  if(sunrise != ''):
+   #     print "sunrise = "+ sunrise
+    #    break
